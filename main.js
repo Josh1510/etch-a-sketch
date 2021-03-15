22cf523cc82d7e13ef7b451334b012a591f6b4b4
josh1510 <joshmoon.1510@gmail.com>
2021-03-15 21:05:35 +1100

    moving grid into a container to better space it on the page

--- main.js
+++ main.js
@@ -12,16 +12,16 @@
 //     bodySelector.append(etchSquare);
 // }
 
-const container = document.getElementById("container");
+const container = document.getElementById('grid-container');
 
 function makeRows(rows, cols) {
-  container.style.setProperty("--grid-rows", rows);
-  container.style.setProperty("--grid-cols", cols);
-  for (c = 0; c < rows * cols; c++) {
-    let cell = document.createElement("div");
-    cell.innerText = c + 1;
-    container.appendChild(cell).className = "grid-item";
-  }
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (i = 0; i < rows * cols; i++) {
+        let cell = document.createElement('div');
+        cell.innerText = i + 1;
+        container.appendChild(cell).className = 'grid-item';
+    }
 }
 
 makeRows(16, 16);
