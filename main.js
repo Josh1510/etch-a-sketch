fdffb24a4348a1dbb11b77c4add5d301e9d4e6fb
josh1510 <joshmoon.1510@gmail.com>
2021-03-20 19:37:35 +1100

    added event listener to add class when cell passed over. added reset button to remove change-color class from all cells when clicked

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
-// const bodySelector = document.querySelector("body");
-
-// for (i = 0; i < 16; i++){
-
-//     for (j = 0; j < 16; j++){
-
-//     }
-// }
-
-// function createSquare(){
-//     var etchSquare = document.createElement('div');
-//     bodySelector.append(etchSquare);
-// }
-
 const container = document.getElementById('grid-container');
+const resetButton = document.getElementById('reset');
 
+let cellArray = [];
+
+//Function to create the grid
 function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (i = 0; i < rows * cols; i++) {
         let cell = document.createElement('div');
+        cell.addEventListener('mouseover', function () {
+            this.classList.add('change-color');
+        });
+        cellArray.push(cell);
         cell.innerText = i + 1;
         container.appendChild(cell).className = 'grid-item';
     }
 }
 
 makeRows(16, 16);
+
+resetButton.addEventListener('click', removeColor);
+
+function removeColor() {
+    for (let i = 0; i < cellArray.length; i++) {
+        cellArray[i].classList.remove('change-color');
+    }
+}
