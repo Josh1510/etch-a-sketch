af9db63eaaa0e7a0f720fa14d9429d5fe831a35b
Joshua Moon <joshmoon.1510@gmail.com>
2021-03-24 22:53:24 +1100

    removing text from each cell

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
-const container = document.getElementById('grid-container');
-const resetButton = document.getElementById('reset');
+const container = document.getElementById("grid-container");
+const resetButton = document.getElementById("reset");
 
 let cellArray = [];
 
 //Function to create the grid
 function makeRows(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (i = 0; i < rows * cols; i++) {
-        let cell = document.createElement('div');
-        cell.addEventListener('mouseover', function () {
-            this.classList.add('change-color');
-        });
-        cellArray.push(cell);
-        cell.innerText = i + 1;
-        container.appendChild(cell).className = 'grid-item';
-    }
+  container.style.setProperty("--grid-rows", rows);
+  container.style.setProperty("--grid-cols", cols);
+  for (i = 0; i < rows * cols; i++) {
+    let cell = document.createElement("div");
+    cell.addEventListener("mouseover", function () {
+      this.classList.add("change-color");
+    });
+    cellArray.push(cell);
+    // cell.innerText = i + 1;
+    container.appendChild(cell).className = "grid-item";
+  }
 }
 
 makeRows(16, 16);
 
-resetButton.addEventListener('click', removeColor);
+resetButton.addEventListener("click", removeColor);
 
 function removeColor() {
-    for (let i = 0; i < cellArray.length; i++) {
-        cellArray[i].classList.remove('change-color');
-    }
+  for (let i = 0; i < cellArray.length; i++) {
+    cellArray[i].classList.remove("change-color");
+  }
 }
