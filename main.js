58bd62bdd359c133fd8531cff6c3c9511d0beacb
josh1510 <joshmoon.1510@gmail.com>
2021-03-27 19:48:26 +1100

    added additional getElement for slider, makeNewGrid function to remove then build new grid based on slider, loaded a few different methods to make the new grid to see what has better performance

--- main.js
+++ main.js
@@ -1,29 +1,55 @@
-const container = document.getElementById("grid-container");
-const resetButton = document.getElementById("reset");
+const container = document.getElementById('grid-container');
+const resetGridButton = document.getElementById('reset-grid');
+const newGridButton = document.getElementById('new-grid');
+const gridSize = document.getElementById('size-slider');
+const sizeLabel = document.getElementById('size-label');
 
 let cellArray = [];
 
 //Function to create the grid
-function makeRows(rows, cols) {
-  container.style.setProperty("--grid-rows", rows);
-  container.style.setProperty("--grid-cols", cols);
-  for (i = 0; i < rows * cols; i++) {
-    let cell = document.createElement("div");
-    cell.addEventListener("mouseover", function () {
-      this.classList.add("change-color");
-    });
-    cellArray.push(cell);
-    // cell.innerText = i + 1;
-    container.appendChild(cell).className = "grid-item";
-  }
+function makeGrid(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (i = 0; i < rows * cols; i++) {
+        let cell = document.createElement('div');
+        cell.addEventListener('mouseover', function () {
+            this.classList.add('change-color');
+        });
+        cellArray.push(cell);
+        // cell.innerText = i + 1;
+        container.appendChild(cell).className = 'grid-item';
+    }
 }
 
-makeRows(16, 16);
+//draw initial grid
+makeGrid(16, 16);
 
-resetButton.addEventListener("click", removeColor);
+function makeNewGrid() {
+    document.querySelectorAll('.grid-item').forEach((e) => e.remove());
+    makeGrid(gridSize.value, gridSize.value);
+}
 
+//make a new grid based on slider size
+newGridButton.addEventListener('click', function () {
+    document.querySelectorAll('.grid-item').forEach((e) => e.remove());
+    makeGrid(gridSize.value, gridSize.value);
+});
+
+//reset the grid when the reset button is clicked
+resetGridButton.addEventListener('click', removeColor);
 function removeColor() {
-  for (let i = 0; i < cellArray.length; i++) {
-    cellArray[i].classList.remove("change-color");
-  }
+    for (let i = 0; i < cellArray.length; i++) {
+        cellArray[i].classList.remove('change-color');
+    }
 }
+
+//set and show the size of the current grid
+sizeLabel.textContent = gridSize.value;
+gridSize.addEventListener('mousemove', function () {
+    sizeLabel.textContent = gridSize.value;
+    // setTimeout(makeNewGrid, 100);
+    makeNewGrid();
+});
+gridSize.addEventListener('mouseup', function () {
+    //makeNewGrid();
+});
