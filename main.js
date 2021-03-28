3f274a3a79a83816e13764025c7aeaaf32a84d67
josh1510 <joshmoon.1510@gmail.com>
2021-03-28 16:13:02 +1100

    changing gridSize event listener from mousemove to input to prevent issue where moving mouse over slider reset grid

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
 const container = document.getElementById('grid-container');
 const resetGridButton = document.getElementById('reset-grid');
-// const newGridButton = document.getElementById('new-grid');
 const gridSize = document.getElementById('size-slider');
 const sizeLabel = document.getElementById('size-label');
 
 let cellArray = [];
 
 //Function to create the grid
-function makeGrid(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (i = 0; i < rows * cols; i++) {
+function makeGrid(gridsize) {
+    container.style.setProperty('--grid-rows', gridsize);
+    container.style.setProperty('--grid-cols', gridsize);
+    for (i = 0; i < gridsize * gridsize; i++) {
         let cell = document.createElement('div');
         cell.addEventListener('mouseover', function () {
             this.classList.add('change-color');
         });
         cellArray.push(cell);
-        // cell.innerText = i + 1;
         container.appendChild(cell).className = 'grid-item';
     }
 }
@@ -26,14 +24,8 @@ makeGrid(16, 16);
 
 function makeNewGrid() {
     document.querySelectorAll('.grid-item').forEach((e) => e.remove());
-    makeGrid(gridSize.value, gridSize.value);
+    makeGrid(gridSize.value);
 }
-
-//make a new grid based on slider size
-// newGridButton.addEventListener('click', function () {
-//     document.querySelectorAll('.grid-item').forEach((e) => e.remove());
-//     makeGrid(gridSize.value, gridSize.value);
-// });
 
 //reset the grid when the reset button is clicked
 resetGridButton.addEventListener('click', removeColor);
@@ -45,11 +37,8 @@ function removeColor() {
 
 //set and show the size of the current grid
 sizeLabel.textContent = gridSize.value;
-gridSize.addEventListener('mousemove', function () {
+
+gridSize.addEventListener('input', function () {
     sizeLabel.textContent = gridSize.value;
-    // setTimeout(makeNewGrid, 100);
     makeNewGrid();
-});
-gridSize.addEventListener('mouseup', function () {
-    //makeNewGrid();
 });
